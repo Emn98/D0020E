d23db3046694bc7e4a6952667b81040cce25b68a
Emn98 <emilnyberg1998@gmail.com>
2022-03-09 18:10:37 +0100

    fixed search for when the user presses enter

--- src/AdminPage/Scripts/search.js
+++ src/AdminPage/Scripts/search.js
@@ -1,4 +1,3 @@
-
 const search_inp = document.querySelector("[policy-search]")
 
 //Run this funtion everytime the user types in the search bar
@@ -33,3 +32,43 @@ search_inp.addEventListener("input", e => {
   
 
 })
+
+
+//If the users pressess enter 
+$(document).ready(function () {
+  $(".search_bar_form").on("submit", function (event) {
+    event.preventDefault();
+
+    var table;
+    var search_by;
+    var search_word;
+
+    //Get value from search bar and then clear it
+    var search_word = document.getElementById("myInput").value;
+    document.getElementById("myInput").value = "";
+
+    /*Get the current table by determining which background color the... 
+     "show all policies button" have */
+    if (show_all_policies_btn.style.backgroundColor == "rgb(0, 136, 169)") {
+      table = "all_policies";
+    } else {
+      table = "loaded_policies";
+    }
+
+    //Determine which checkbox have been checked
+    if ($(".policy_name_check:checked").val() == "on") {
+      search_by = "Policy_name";
+    } else if ($(".user_check:checked").val() == "on") {
+      search_by = "Users";
+    } else {
+      search_by = "Objects";
+    }
+
+    //Send search_word, search_by and table infor to the backend.
+    $("#t_body_main_page").load("search_backend.php", {
+      table: table,
+      search_by: search_by,
+      search_word: search_word,
+    });
+  });
+});
--- src/AdminPage/Scripts/search_for_users_objects_overlay.js
+++ src/AdminPage/Scripts/search_for_users_objects_overlay.js
@@ -15,3 +15,27 @@ search_input.addEventListener("input", e => {
   );
 
 })
+
+//If the user pressess enter. 
+$(document).ready(function () {
+  $("#admin_man_search_form").on("submit", function (event) {
+    event.preventDefault();
+
+    var current_table;
+    var search_word;
+
+    //Retrive which table the user is looking at(i.e Users or Objects)
+    current_table = document.getElementById("admin_man_title").innerHTML;
+
+    //Retrive the search_word
+    search_word = document.getElementById("admin_man_search_inp").value;
+    document.getElementById("admin_man_search_inp").value = "";
+
+    $("#admin_man_table_body").load("/AdminPage/Overlays/UsersAndObjectsOverlay/search_users_objects_overlay.php",
+      {
+        current_table: current_table,
+        search_word: search_word,
+      }
+    );
+  });
+});
